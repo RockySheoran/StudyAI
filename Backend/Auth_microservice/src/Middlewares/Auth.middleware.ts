1c20cfbb2ccf60b857f502c9b3effe9c78420382
Rocky sheoran <rockysheoran72@gmail.com>
2025-07-23 23:41:44 +0530

    auth middleware make

--- Backend/Auth_microservice/src/Middlewares/Auth.middleware.ts
+++ Backend/Auth_microservice/src/Middlewares/Auth.middleware.ts
@@ -27,7 +27,7 @@ export const middleware = (req: AuthenticatedRequest, res: Response, next: NextF
   // Check authorization header
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1];
-  } 
+  }
   // Check cookies
   else if (req.cookies?.token) {
     token = req.cookies.token;
