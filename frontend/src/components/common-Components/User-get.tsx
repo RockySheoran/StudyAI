6b0cbe0197c9fc0a2cda96d60e1455bc2f9266bd
Rocky sheoran <rockysheoran72@gmail.com>
2025-08-26 00:36:10 +0530

    change in get user

--- frontend/src/components/common-Components/User-get.tsx
+++ frontend/src/components/common-Components/User-get.tsx
@@ -8,11 +8,7 @@ import { toast } from "sonner";
 
 const User_get = ({ initialToken, loading, setLoading }: { initialToken?: string, loading?: boolean, setLoading: (loading: boolean) => void }) => {
   const { setToken, setProfile, name, email, clearUser } = useUserStore();
-<<<<<<< HEAD
-console.log(initialToken ,"vmkfd vkdf")
-=======
-  console.log("vbyuhfvbwkvfirst", initialToken)
->>>>>>> f45cdcc1af4564c3d5ba7c3f7d1c02532fafca47
+
   // Initialize token once when component mounts
   useEffect(() => {
     if (initialToken) {
@@ -23,12 +19,11 @@ console.log(initialToken ,"vmkfd vkdf")
 
   // Fetch user profile data
   useEffect(() => {
-<<<<<<< HEAD
+
     if (initialToken && !name) {
       console.log(initialToken)
-=======
-    if (initialToken ) {
->>>>>>> f45cdcc1af4564c3d5ba7c3f7d1c02532fafca47
+
+  
       const getMe = async () => {
         try {
           console.log("first" , initialToken)
